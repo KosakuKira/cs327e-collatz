8c8f54f32da5067767baa3f1082b0fcddddceb96
KosakuKira <jordanong1@gmail.com>
2014-01-26 16:40:31 -0600

    Text tests removed.
    Used default RunCollatz.py stuff instead

--- RunCollatz.py
+++ RunCollatz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# ------------------------------
+# -------------------------------
 # projects/collatz/RunCollatz.py
 # Copyright (C) 2014
 # Glenn P. Downing
@@ -28,22 +28,6 @@ from Collatz import collatz_solve
 # main
 # ----
 
-#def main():
- #   fileNails = open('RunCollatz.txt', 'r')
-  #  fileJony = open('RunCollatz.out', 'w')
-    
-   # for nails in fileNails:
-        # nails = str(nails, encoding = 'utf8')
-    #    nails = nails.rstrip("\n")
-     #   print (nails)
-    
-    
-    #jony = fileJony.read()
-    
-    
-    #fileNails.close()
-    #fileJony.close()
 
-collatz_solve('RunCollatz.txt', 'RunCollatz.out')
+collatz_solve(sys.stdin, sys.stdout)
 
-#main()
